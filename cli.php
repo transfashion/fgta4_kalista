8a8b011d0af06b0c71a4c6341409c6dd9869e2eb
agungdhewe <agung_dhewe@yahoo.com>
2024-09-05 16:48:28 +0700

    fix cli for direct access

--- cli.php
+++ cli.php
@@ -3,15 +3,25 @@ define('FGTA4', 1);
 
 date_default_timezone_set('Asia/Jakarta');
 
+
+if (!defined('__CONFIG_PATH')) {
+	define('__CONFIG_PATH', dirname(__FILE__) . '/public/config.php');
+}
+
 if (!defined('__ROOT_DIR')) {
 	define('__ROOT_DIR', dirname(__FILE__));
 }
 
 
+
+
+
 require_once __CONFIG_PATH;
-require_once __ROOT_DIR.'/core/webexception.php';
-require_once __ROOT_DIR.'/core/vendor/autoload.php';
+require_once __ROOT_DIR . '/core/webexception.php';
 require_once __ROOT_DIR . '/core/dbsetting.php';
+
+
+require_once __ROOT_DIR.'/vendor/autoload.php';
 
 
 if (!defined('__LOCAL_CURR')) {
@@ -53,7 +63,7 @@ class console {
 			$args = self::getcommandparameter($argv);
 			$cmd = self::loadcommand($args->command, $args);
 
-		} catch (Exception $ex) {
+		} catch (\Exception $ex) {
 			echo "\r\n";
 			echo color::red . "ERROR\r\n=====" . color::reset , "\r\n";
 			echo $ex->getMessage();
@@ -65,7 +75,7 @@ class console {
 	public static function getcommandparameter($argv) {
 		try {
 			if (count($argv)<2) {
-				throw new Exception("perintah belum didefinisikan" . self::format);
+				throw new \Exception("perintah belum didefinisikan" . self::format);
 			}
 
 			$params = [];
@@ -87,7 +97,7 @@ class console {
 				'command' => $argv[1],
 				'params' => $params
 			);
-		} catch (Exception $ex) {
+		} catch (\Exception $ex) {
 			throw $ex;
 		}
 	}
@@ -100,29 +110,28 @@ class console {
 			$command_path = __ROOT_DIR . "/apps/" . $command_dir;
 			
 			if (!is_file($command_path)) {
-				throw new Exception(color::yellow . $command_dir . color::reset . " tidak ditemukan.");
+				echo $command_path;
+				throw new \Exception(color::yellow . $command_dir . color::reset . " tidak ditemukan.");
 			}
 
 			if (!defined('DB_CONFIG')) {
-				throw new Exception('Konfigurasi database belum di-set');
+				throw new \Exception('Konfigurasi database belum di-set');
 			}
 
 			if (!is_array(DB_CONFIG) || !is_array(DB_CONFIG_PARAM)) {
-				throw new Exception('Konfigurasi database belum di-set');
+				throw new \Exception('Konfigurasi database belum di-set');
 			}
 
 			require_once $command_path;
 
 
-		} catch (Exception $ex) {
+		} catch (\Exception $ex) {
 			throw $ex;
 		}
 	}
 
-	public static function require($filename) {
-
-	}
-
+	// public static function require($filename) {
+	// }
 
 	public static function class($obj) {
 		$obj->execute();
